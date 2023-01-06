d3965da5b7e1da99ca1a86ea6bb89c3299712199
ziu7wx <jin.zhu5@cn.bosch.com>
2023-01-06 20:48:58 +0800

    feat: upload file with filename

--- src/views/RQM/upload.vue
+++ src/views/RQM/upload.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
-        <el-upload class="upload-demo" drag action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
-            multiple>
+        <el-upload ref="uploadRef" v-model:file-list="fileList" class="upload-demo" drag action="/api/rqm/upload"
+            :headers="header" :autoUpload="false" :on-remove="handleRemove" :on-preview="handlePreview"
+            :on-success="handleSuccess">
             <el-icon class="el-icon--upload"><upload-filled /></el-icon>
             <div class="el-upload__text">
                 Drop file here or <em>click to upload</em>
@@ -10,13 +11,54 @@
                 <div class="el-upload__tip">
                     jpg/png files with a size less than 500kb
                 </div>
+                <el-button class="ml-3" type="success" @click="submitUpload">
+                    upload to server
+                </el-button>
             </template>
         </el-upload>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref, reactive } from 'vue'
 import { UploadFilled } from '@element-plus/icons-vue'
+import { getToken } from '@/utils/auth';
+import type { UploadProps, UploadUserFile, UploadInstance } from 'element-plus'
+import { ElMessageBox, ElMessage } from 'element-plus'
+
+const fileList = ref<UploadUserFile[]>()
+const uploadRef = ref<UploadInstance>()
+const token = getToken()
+const header = reactive({
+    Authorization: `Bearer ${token}`,
+    "Content-Disposition": "attachment; filename=source.json"
+})
+
+const handleRemove: UploadProps['onRemove'] = (file, uploadFiles) => {
+    console.log(file.name, uploadFiles)
+}
+
+const handlePreview: UploadProps['onPreview'] = (uploadFile) => {
+    console.log(uploadFile)
+}
+
+const handleSuccess: UploadProps['onSuccess'] = () => {
+    console.log();
+}
+
+const submitUpload = () => {
+    if (fileList.value) {
+        let tmp = <UploadUserFile[]>fileList.value.slice()
+        fileList.value = []
+        tmp.forEach((file) => {
+            fileList.value?.push(file)
+            header['Content-Disposition'] = `attachment; filename=${file.name}`
+            uploadRef.value!.submit()
+            fileList.value?.pop()
+        })
+        fileList.value = tmp.slice()
+    }
+}
 </script>
 
 <style scoped>
